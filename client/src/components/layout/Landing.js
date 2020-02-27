c7fa3b35c0d12bca5f1dff3781234223afe0b313
KA <kareemalasmar@gmail.com>
2020-02-26 18:20:08 -0800

    initial navbar and landing component

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Board Meeting</h1>
+          <p className='lead'>
+            Create a snowbaording profile, share posts and get help from other
+            boarders
+          </p>
+          <div className='buttons'>
+            <a href='register.html' className='btn btn-primary'>
+              Sign Up
+            </a>
+            <a href='login.html' className='btn btn-light'>
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <a href='index.html'>
+          <i className='far fa-snowflake'></i> Board Meeting
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href='profiles.html'>Boarders</a>
+        </li>
+        <li>
+          <a href='register.html'>Register</a>
+        </li>
+        <li>
+          <a href='login.html'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
