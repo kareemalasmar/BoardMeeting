1978b11bd3149299b1ad609aab05ada9d8104f1b
KA <kareemalasmar@gmail.com>
2020-03-12 04:12:39 -0700

    view my profile button on dashboard and logout redirect

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const DashboardActions = () => {
+const DashboardActions = ({
+  auth: {
+    user: { _id }
+  }
+}) => {
   return (
     <div className='dash-buttons'>
       <Link to='/edit-profile' className='btn btn-light'>
         <i className='fas fa-user-circle text-primary'></i> Edit Profile
       </Link>
+      <Link to={`/profile/${_id}`} className='btn btn-primary'>
+        View Profile
+      </Link>
     </div>
   );
 };
 
-export default DashboardActions;
+DashboardActions.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(DashboardActions);
