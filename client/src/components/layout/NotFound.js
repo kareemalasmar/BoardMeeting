c0530c8cee66977a21397e6970cd0a381e61e7f9
KA <kareemalasmar@gmail.com>
2020-03-17 17:10:43 -0700

    landing page img and not found style

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,13 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 const NotFound = () => {
   return (
-    <Fragment>
-      <h1 className='x-large text-primary'>
-        <i className='fas fa-exclamation-triangle'></i> Page Not Found
-      </h1>
-      <p className='large'>Sorry, this page does not exist</p>
-    </Fragment>
+    <section className='not-found'>
+      <div className='dark-overlay'>
+        <div className='not-found-inner'>
+          <h1 className='x-large text-primary'>
+            <i className='fas fa-exclamation-triangle'></i> Page Not Found
+          </h1>
+          <p className='large'>Sorry, this page does not exist</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
