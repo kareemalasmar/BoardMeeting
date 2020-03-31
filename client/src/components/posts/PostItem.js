e67259215f44e6e96b822d06f46f2197a0526bb5
KA <kareemalasmar@gmail.com>
2020-03-30 20:43:30 -0700

    experience prop type fixed

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -43,7 +43,7 @@ const PostItem = ({
             <i className='fas fa-thumbs-down'></i>
           </button>
           <Link to={`/posts/${_id}`} className='btn btn-primary'>
-            <i class='far fa-comments'></i> Discussion{' '}
+            <i className='far fa-comments'></i> Discussion{' '}
             {comments.length > 0 && (
               <span className='comment-count'>{comments.length}</span>
             )}
